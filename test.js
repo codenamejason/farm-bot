2f977b93bdec4bd6565b58898951a8d919947aaf
codenamejason <codenamejason@outlook.com>
2020-09-25 14:12:07 -0400

    working version

---
+++ test.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const http = require('http')
+const Web3 = require('web3')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const moment = require('moment-timezone')
+const numeral = require('numeral')
+const _ = require('lodash')
+const axios = require('axios')
+
+// SERVER CONFIG
+const PORT = process.env.PORT || 5000
+const app = express();
+const server = http.createServer(app).listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// WEB3 CONFIG
+const web3 = new Web3(process.env.RPC_URL)
+
