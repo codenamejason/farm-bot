276187a11601ff9e1a810fb572c9299f8f210000
codenamejason <codenamejason@outlook.com>
2021-07-10 15:30:03 -0400

    testing

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 const bn = require("bn.js");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-const Network = require("@maticnetwork/meta/network");
+const Network = require("@maticnetwork/meta");
 const Matic = require("@maticnetwork/maticjs").default;
 
 const SCALING_FACTOR = new bn(10).pow(new bn(18));
